115a47fa2293b682ec59afae56936174667c1955
Victor Guyard <19635051+FlipFloop@users.noreply.github.com>
2022-06-11 17:12:27 -0400

    ughhrubgebrfe

---
+++ src/routes.ts
@@ -0,0 +1,17 @@
+import { lazy } from "solid-js";
+import type { RouteDefinition } from 'solid-app-router';
+
+export const routes: RouteDefinition[] = [
+  {
+    path: '/',
+    component: lazy(() => import("./screens/main")),
+  },
+  {
+    path: '',
+    component: lazy(() => import('./screens/main/Home')),
+  },
+  {
+    path: '/:meetCode',
+    component: lazy(() => import("./screens/Meeting")),
+  },
+];
